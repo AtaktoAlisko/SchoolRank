19c8e89d7fedc3bc9b63d0d39e3011cd25a79ee6
Алибек Мурат <alibekmurat@MBP-Alibek.lan>
2025-04-07 23:34:18 +0500

    initial commit

--- driver/driver.go
+++ driver/driver.go
@@ -8,26 +8,30 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 // ConnectDB устанавливает подключение к базе данных
 func ConnectDB() *sql.DB {
 	var err error
+	var dbURL string
 
-	// Получаем URL базы данных из переменной окружения
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL == "" {
-		log.Fatal("DATABASE_URL не задана")
+	// Проверяем, есть ли переменная окружения JAWSDB_URL (для Heroku)
+	if os.Getenv("JAWSDB_URL") != "" {
+		// Если переменная окружения есть, используем её
+		dbURL = os.Getenv("JAWSDB_URL")
+	} else {
+		// Если переменной окружения нет, подключаемся к локальной базе данных
+		dbURL = "root:Zhanibek321@tcp(127.0.0.1:3306)/my_database"
 	}
 
-	// Открытие подключения
-	db, err := sql.Open("mysql", dbURL)
+	// Открываем подключение к базе данных
+	db, err = sql.Open("mysql", dbURL)
 	if err != nil {
 		log.Fatal("Ошибка подключения к базе данных:", err)
 	}
-
-	// Проверяем подключение
+	// Проверяем, что подключение успешное
 	if err := db.Ping(); err != nil {
 		log.Fatal("Не удалось подключиться к базе данных:", err)
 	}
-
 	return db
 }
