9ef7bd3a297e0797ac5444a948ba8b7510534db8
kamilakazikhanova <anarab902@gmail.com>
2025-06-07 21:29:52 +0500

    modified:   models/add_event.go

--- models/add_event.go
+++ models/add_event.go
@@ -3,20 +3,20 @@ package models
 type Event struct {
 	ID                int    `json:"id"`
 	SchoolID          int    `json:"school_id"`
-	SchoolName        string `json:"school_name"`// S
+	SchoolName        string `json:"school_name"` // S
 	UserID            int    `json:"user_id"`
-	EventName         string `json:"event_name"`// S
+	EventName         string `json:"event_name"` // S
 	Description       string `json:"description"`
-	Photo             string `json:"photo"`// S
-	Participants      int    `json:"participants"`// S
-	Limit             int    `json:"limit"`// S
+	Photo             string `json:"photo"`        // S
+	Participants      int    `json:"participants"` // S
+	Limit             int    `json:"limit"`        // S
 	LimitParticipants int    `json:"limit_participants"`
-	StartDate         string `json:"start_date"`// S
-	EndDate           string `json:"end_date"`// S
-	Location          string `json:"location"`// S
+	StartDate         string `json:"start_date"` // S
+	EndDate           string `json:"end_date"`   // S
+	Location          string `json:"location"`   // S
 	Grade             int    `json:"grade"`
 	CreatedAt         string `json:"created_at"`
 	UpdatedAt         string `json:"updated_at"`
 	CreatedBy         string `json:"created_by"`
-	Category          string `json:"category"` 
+	Category          string `json:"category"`
 }
